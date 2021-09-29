4c432ae64adfc9d285c075bfba7d0f7c424c8d50
alunatic94 <Alberto.luna.642@my.csun.edu>
2021-09-29 16:16:28 -0700

    displaying map using leaflet library

--- script.js
+++ script.js
@@ -16,7 +16,18 @@ if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(function(position){
         const {latitude}=position.coords;
         const {longitude}=position.coords;
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+        const coords=[latitude, longitude];
+
+        const map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
     },function(){
         alert('could not get your position');
     })
