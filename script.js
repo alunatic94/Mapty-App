1c9fe489fa5196b35fe1836f0067014e6c9f4603
alunatic94 <Alberto.luna.642@my.csun.edu>
2021-10-04 22:51:32 -0700

    finished marker on click

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 class Workout{
     date=new Date();
     id=(Date.now()+'').slice(-10);
+    clicks=0;
     constructor(coords,distance,duration){
         this.coords=coords;//[lat,lng]
         this.distance=distance;//in km
@@ -18,6 +19,9 @@ class Workout{
         
         this.description=`${this.type[0].toUpperCase()}`+`${this.type.slice(1)}
             on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
+    click(){
+        this.click++;
     }
 }
 class Running extends Workout{
@@ -65,6 +69,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class App{
     //Private properties
     #map;
+    #mapZoomLevel=13;
     #mapEvent;
     #workouts=[];
     constructor(){
@@ -73,6 +78,7 @@ class App{
         form.addEventListener('submit',this._newWorkout.bind(this));
         
         inputType.addEventListener('change',this._toggleElevationField);
+        containerWorkouts.addEventListener('click',this._moveToPopup.bind(this));
     }
     _getPosition(){
         if(navigator.geolocation){
@@ -90,7 +96,7 @@ class App{
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
         const coords=[latitude, longitude];
     
-        this.#map = L.map('map').setView(coords, 13);
+        this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
     
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -226,6 +232,23 @@ class App{
             `;
         }
         form.insertAdjacentHTML('afterend',html);
+    }
+
+    _moveToPopup(e){
+        const workoutEl=e.target.closest('.workout');
+        if(!workoutEl)return;
+        const workout=this.#workouts.find(
+            work=>work.id===workoutEl.dataset.id
+        );
+        this.#map.setView(workout.coords,this.#mapZoomLevel,{
+            animate:true,
+            pan:{
+                duration:1,
+            },
+        });
+
+        //using the public interface
+        workout.click();
     }
 }
 
